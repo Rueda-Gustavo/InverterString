b493941ba54b7d2cd413de24b71fafd9f3e6a09d
Rueda-Gustavo <78812664+Rueda-Gustavo@users.noreply.github.com>
2023-01-14 16:02:19 -0300

    Add files via upload

---
+++ InverterString/InverterString/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string exemplo = "Teste desenvolvedor.";        
+        Console.WriteLine("String de exemplo para ser invertida: " + exemplo);
+        Console.WriteLine("String invertida: " + inverterString(exemplo));
+
+        Console.WriteLine("Digite uma string para ser invertida: ");
+        string input = Console.ReadLine();
+        Console.WriteLine("String invertida: " + inverterString(input));
+
+
+        
+    }
+
+    private static string inverterString(string normal)
+    {
+        string inversa = normal;
+
+        if (normal.Length > 1)
+        {
+            inversa = String.Empty;            
+            for(int i = normal.Length - 1; i >= 0; i--)
+            {
+                inversa += normal[i];
+            }
+        }
+
+        return inversa;
+    }
+}
